de62a7bd69568695cdcabb4d43f061b5a1874684
Sanju Dhritlahre <88761391+sanjudhritlahre@users.noreply.github.com>
2022-09-13 20:13:48 +0530

    Program 02

---
+++ sum.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int a, b;
+
+    printf("Enter the first number a: ");
+    scanf("%d", &a);
+
+    printf("Enter the second number b: ");
+    scanf("%d", &b);
+
+    printf("Your entered number of sum is: %d", a + b);
+
+    return 0;
+}
