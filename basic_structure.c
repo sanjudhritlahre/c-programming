577210964a769b079e87911a099032e7065fabdf
Sanju Dhritlahre <88761391+sanjudhritlahre@users.noreply.github.com>
2022-09-13 20:42:34 +0530

    Basic Structures Of C

---
+++ basic_structure.c
@@ -0,0 +1,19 @@
+/* Structure of C Language 13-08-2022 by Sanju - (Documentation Section) */
+
+#include <stdio.h> // Linking Section
+
+    int a;   // Data Type & Varaiables (Declaration Section)
+    a = 100; // Definitions
+
+void main() // Main Function Declaration Section
+
+{
+    // Local Variable Declaration
+    // Method Or Function Definitions
+}
+
+
+// User define Functions or Sub-Functions
+myFunction() {
+
+}
