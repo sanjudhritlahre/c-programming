7aede90143fa6f678e6197154bf3dc23803b6189
Sanju Dhritlahre <88761391+sanjudhritlahre@users.noreply.github.com>
2022-10-19 19:40:11 +0530

    Function Program 02

---
+++ function01.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// Function Prototype
+int sum(int a, int b);
+
+int main(void)
+{
+
+    int a, b, c;
+    a = 56;
+    b = 98;
+    c = sum(a, b); // call the function
+    printf("The sum is: %d", c);
+
+    return 0;
+}
+
+// Creat a function
+int sum(int a, int b)
+{
+    return a + b; // code to be executed
+}
+
+// Outputs "The sum is: 154"
